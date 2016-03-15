9bc979d750f90d7693a5febac3ec615a719aad42
liuzhao <63993048@qq.com>
2016-03-15 17:06:16 +0800

    初次提交

---
+++ src/com/onroad/index/IndexController.java
@@ -0,0 +1,12 @@
+package com.onroad.index;
+
+import com.jfinal.core.Controller;
+
+/**
+ * IndexController
+ */
+public class IndexController extends Controller {
+	public void index() {
+		render("index.html");
+	}
+}
